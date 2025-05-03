9275372af0b7ee7de052b950ebaf4d927541b7ac
Menne Bos <mennebos@gmail.com>
2025-05-03 21:47:05 +0200

    Fix URL in setup.py and update resource directories in find_resource_files function

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import walk, path
 from os.path import join, dirname
 # from setuptools import setup, find_packages
 
-URL = "https://github.com/MenneBos/ovos-skill-HomeyflowTrigger"
+URL = "https://github.com/MenneBos/skill-ovos-HomeyflowTrigger"
 SKILL_CLAZZ = "HomeyFlowSkill"  # needs to match __init__.py class name
 PYPI_NAME = "ovos-skill-HomeyFlowTrigger"  # pip install PYPI_NAME
 
@@ -51,7 +51,7 @@ def get_version():
     return version
 
 def find_resource_files():
-    resource_base_dirs = ("locale", "vocab", "intent", "dialog", "regex", "skill")
+    resource_base_dirs = ("locale", "vocab", "nodejs", "intent", "dialog", "regex", "skill")
     base_dir = path.dirname(__file__)
     package_data = ["*.json"]
     for res in resource_base_dirs:
@@ -78,7 +78,7 @@ setup(
     license='Apache-2.0',
     include_package_data=True,
     package_dir={SKILL_PKG: ""},
-    package_data={SKILL_PKG: ['/nodejs/*.json','/nodejs/*.js', 'shared']},
+    package_data={SKILL_PKG: find_resource_files()},
     packages=[SKILL_PKG],
     keywords='ovos skill plugin',
     entry_points={'ovos.plugin.skill': PLUGIN_ENTRY_POINT}
