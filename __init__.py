0dfd66a74858e52e341684ed7ddf504ed1b7e2ae
Menne Bos <mennebos@gmail.com>
2025-07-10 21:36:39 +0200

    Refactor HomeyFlowSkill to implement language detection and set default intent directory for Dutch and English languages.

--- __init__.py
+++ __init__.py
@@ -59,16 +59,17 @@ class HomeyFlowSkill(OVOSSkill):
         self.config_path = os.path.expanduser("~/.config/ovos_skill_homeyflowtrigger/config.json")
         self.config = self._load_config()
 
+        ###### to be solved to run in english mode, standard Dutch ######
         # Language detection
-        #self.language = self.get_default_lang()
+        # self.language = self.get_default_lang()
+        self.language = "nl-nl"
         #self.log.info(f"✅ Detected language: {self.language}")
-        #if self.language.lower() == "nl-nl":
-        #    self.intent_dir = os.path.join(self.root_dir, "locale", "nl-nl", "intents")
-        #elif self.language.lower() == "en-us":
-        #    self.intent_dir = os.path.join(self.root_dir, "locale", "en-us", "intents")
-        #else:
-        #    self.log.error("❌ No valid langauge (nl-nl or en-us detected in mycroft.conf).")
-        self.intent_dir = os.path.join(self.root_dir, "locale", "nl-nl", "intents")
+        if self.language.lower() == "nl-nl":
+            self.intent_dir = os.path.join(self.root_dir, "locale", "nl-nl", "intents")
+        elif self.language.lower() == "en-us":
+            self.intent_dir = os.path.join(self.root_dir, "locale", "en-us", "intents")
+        else:
+            self.log.error("❌ No valid langauge (nl-nl or en-us detected in mycroft.conf).")
 
         # Ensure configuration is loaded before setting up MQTT
         if not self.broker_url:
